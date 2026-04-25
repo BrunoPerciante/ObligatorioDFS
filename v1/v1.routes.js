4699e0661a322dd406fa1a75d98ba4ae0d1ed9de
BrunoPerciante <bp334880@fi365.com.edu.uy>
2026-04-24 22:27:41 -0300

    cloudynariimagen

--- v1/v1.routes.js
+++ v1/v1.routes.js
@@ -4,12 +4,14 @@ import usuariosRouter from "./routes/usuario.routes.js";
 import authRouter from "./routes/auth.routes.js";
 
 import { authenticateMiddleware } from "./middlewares/authenticate.middleware.js";
+import uploadsRouter from "./routes/uploads.routes.js"
 
 const router = express.Router({mergeParams:true});
 
 //peticiones que llegan a /v1
 //rutas de login y registro de usuarios, sin necesidad de token
 router.use("/auth", authRouter);
+router.use("/uploads",uploadsRouter);
 
 //middleware para verificar de token
 router.use(authenticateMiddleware);
