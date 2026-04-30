b052430a8cd6c2046109e64fac60412807b3b9a0
BrunoPerciante <bp334880@fi365.com.edu.uy>
2026-04-30 19:33:17 -0300

    obligatorio ahorasi si

--- v1/routes/ai.routes.js
+++ v1/routes/ai.routes.js
@@ -1,9 +1,10 @@
 import express from "express";
+import {authenticateMiddleware} from "../middlewares/authenticate.middleware.js"
 import { getModels, useGemini25Flash } from "../controllers/ai.controller.js";
 
 const router = express.Router({ mergeParams: true });
 
-router.get("/", getModels);
-router.post("/", useGemini25Flash);
+router.get("/",authenticateMiddleware, getModels);
+router.post("/", authenticateMiddleware,useGemini25Flash);
 
 export default router;
--- v1/routes/uploads.routes.js
+++ v1/routes/uploads.routes.js
@@ -1,8 +1,9 @@
 import express from "express";
+import {authenticateMiddleware} from "../middlewares/authenticate.middleware.js"
 import{subirImagen} from "../controllers/uploads.controller.js";
 
 const router = express.Router({mergeParams:true});
 
-router.post("/",subirImagen);
+router.post("/",authenticateMiddleware,subirImagen);
 
 export default router;
